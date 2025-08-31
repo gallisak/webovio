494ea44daf682ec8246005d89eea90b1d0959c28
Andrii Halchyshak <agalchisak@gmail.com>
2025-08-31 21:41:10 +0300

    sixth commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("button");
 const menu = document.querySelector(".burger-menu");
 const menu_a = document.getElementById("menu_a");
+const mobile_btn = document.querySelector(".mobile-btn");
 
 button.addEventListener("click", () => {
   button.classList.toggle("btn_burger_toggle");
@@ -8,5 +9,18 @@ button.addEventListener("click", () => {
 
   setTimeout(() => {
     menu_a.classList.toggle("block");
+  }, 100);
+
+  mobile_btn.classList.remove("none");
+  mobile_btn.classList.add("block");
+});
+
+mobile_btn.addEventListener("click", () => {
+  button.classList.toggle("btn_burger_toggle");
+  menu.classList.remove("burger-menu-show");
+
+  setTimeout(() => {
+    menu_a.classList.toggle("block");
+    mobile_btn.classList.toggle("block");
   }, 100);
 });
